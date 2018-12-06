afb9184d2de5a4f658097b41169ece0a3c6b4295
corentin <corentin.guillard@ynov.com>
2018-12-06 22:13:56 +0100

    Partie xml (page home, mon profil, reservation partie 2) fini. Début de la partie réservation partie 1

--- app/src/main/java/com/corentin/mad_rental/MainActivity.java
+++ app/src/main/java/com/corentin/mad_rental/MainActivity.java
@@ -1,7 +1,11 @@
 package com.corentin.mad_rental;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Typeface maFontBold = Typeface.createFromAsset(getAssets(),
+                "font/Roboto-BlackItalic.ttf");
+        TextView textView = findViewById(R.id.pageHome_title);
+        textView.setTypeface(maFontBold);
+    }
+
+    public void MesReservations(View view) {
+        Intent intent = new Intent(this, Profil.class);
+        startActivity(intent);
+
+    }
+
+    public void MonProfil(View view) {
+        Intent intent = new Intent(this, Profil.class);
+        startActivity(intent);
+    }
+
+    public void Recherche(View view) {
+        Intent intent = new Intent(this, Profil.class);
+        startActivity(intent);
     }
 }
